e80b4363ed49f88163c06c989a359124883b9f65
Christopher Scherb <christopher.scherb@fhnw.ch>
2022-01-20 14:40:30 +0100

    add more texts

---
+++ getProximityGraph.py
@@ -0,0 +1,25 @@
+import angr
+import os, sys
+
+def main(argv):
+    if len(argv) < 2:
+        return
+    
+    prog_name = argv[1]
+    project = angr.Project(prog_name, auto_load_libs=False)
+    res = getProximityGraph(project)
+    print(res)
+    lres = list(res)
+    print(list())
+
+    
+def getProximityGraph(project: angr.Project):
+
+    cfg = project.analyses.CFGFast()
+    entry_func = cfg.kb.functions[project.entry]
+    model = angr.knowledge_plugins.cfg.cfg_model.CFGModel(0)
+    graph = project.analyses.Proximity(entry_func, model, 0)
+    return graph.graph.nodes
+
+if __name__ == "__main__":
+    main(sys.argv)
