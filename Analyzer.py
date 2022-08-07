fe65d3bc21836bb6a9f145d624da8a937a582d83
Christopher Scherb <christopher.scherb@fhnw.ch>
2022-08-08 00:57:29 +0200

    start vulnerability analyzer

--- Analyzer.py
+++ Analyzer.py
@@ -5,7 +5,7 @@ import angr, claripy
 
 class Analyzer: 
 
-    def __init__(self, prog_name, auto_load_libs=False, analyze_uncalled=False):
+    def __init__(self, prog_name: str, auto_load_libs=False, analyze_uncalled=False):
         self.project = angr.Project(prog_name, auto_load_libs=auto_load_libs)
         self.analyze_uncalled = analyze_uncalled #todo if enabled also analyse uncalled functions for weaknesses
         self.cfg = self.project.analyses.CFGFast()
@@ -26,11 +26,11 @@ class Analyzer:
         functionAddresses = list(self.cfg.kb.functions)
         return functionAddresses
 
-    def getEntryFunction(self):
+    def getEntryFunction(self) -> angr.knowledge_plugins.functions.function.Function:
         entry_func = self.cfg.kb.functions[self.project.entry]
         return entry_func
 
-    def printAllCalledFunctions(self, entry: angr.knowledge_plugins.functions.function.Function=None, exclude_sysfunc=False):
+    def printAllCalledFunctions(self, entry: angr.knowledge_plugins.functions.function.Function=None, exclude_sysfunc=False) -> None:
         if entry is None:
             entry = self.getEntryFunction()
         functions =self.getListOfCalledFunctions(entry)
@@ -42,3 +42,23 @@ class Analyzer:
                 if (f.is_syscall or f.is_plt or f.is_simprocedure):
                     continue
             self.printAllCalledFunctions(f, exclude_sysfunc=exclude_sysfunc)
+
+    
+
+class Vulnerability_Analyser:
+
+    def __init__(self, project: angr.Project):
+        self.project = project
+        self.stack_chk_fail_addr = self.project.loader.find_symbol("__stack_chk_fail").rebased_addr
+
+    def check_unconstrained(self, state: angr.SimState):
+        if state.solver.symbolic(state.regs.pc):
+            return True
+        return False
+
+    def stack_smashing_checker(self, state: angr.SimState) -> bool:
+        if state.addr == self.stack_chk_fail_addr:
+            return True
+        return False
+
+
